aee2aeef81b30d6aa7405c16b73e2cc46aaf4518
Maira Bello <maira.araujo@liferay.com>
2015-06-18 14:00:32 -0300

    Creates project to hold all steel components

---
+++ README.md
@@ -0,0 +1,3 @@
+# steel-components
+
+A collection of UI components built on top of Metal.js (JavaScript) and Bootstrap 3 (HTML/CSS).
---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var gulp = require('gulp');
+var metal = require('gulp-metal');
+var path = require('path');
+
+metal.registerTasks({
+	buildSrc: 'bower_components/steel-*/src/**/*.js',
+	bundleFileName: 'steel.js',
+	cssSrc: 'bower_components/steel-*/src/**/*.css',
+	globalName: 'steel',
+	scssSrc: 'bower_components/steel-*/src/**/*.scss'
+});
+
+gulp.task('soy', function() {
+	return gulp.src('bower_components/steel-*/src/**/*.soy')
+		.pipe(metal.soy.generateSoy()())
+		.pipe(gulp.dest(function(file) {
+			var index = file.relative.indexOf(path.sep + 'src' + path.sep);
+			file.base = path.join(file.base, file.relative.substr(0, index + 5));
+			return 'build/soy';
+		}));
+});
