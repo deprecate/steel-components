1b8c2860da028a60878a652c7804795453149137
Maira Bello <maira.araujo@liferay.com>
2015-06-26 16:19:42 -0300

    Changes soy task to also compile crystal soy files
    Crystal soy files are compiled first to make sure that steel soy files have preference over them.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var gulp = require('gulp');
+var lazypipe = require('lazypipe');
 var metal = require('gulp-metal');
 var path = require('path');
 
@@ -13,14 +14,24 @@ metal.registerTasks({
 	scssSrc: 'bower_components/steel-*/src/**/*.scss'
 });
 
-gulp.task('soy', function() {
+gulp.task('soy', ['soy:crystal'], function() {
 	return gulp.src('bower_components/steel-*/src/**/*.soy')
-		.pipe(metal.soy.generateSoy()())
-		.pipe(gulp.dest(function(file) {
-			var index = file.relative.indexOf(path.sep + 'src' + path.sep);
-			file.base = path.join(file.base, file.relative.substr(0, index + 5));
-			return 'build/soy';
-		}));
+		.pipe(compileSoy()());
+});
+
+gulp.task('soy:crystal', function() {
+	return gulp.src('bower_components/crystal-*/src/**/*.soy')
+		.pipe(compileSoy()());
 });
 
 gulp.task('default', ['build:globals', 'build:amd', 'build:jquery']);
+
+function compileSoy() {
+	return lazypipe()
+		.pipe(metal.soy.generateSoy())
+		.pipe(gulp.dest, function(file) {
+			var index = file.relative.indexOf(path.sep + 'src' + path.sep);
+			file.base = path.join(file.base, file.relative.substr(0, index + 5));
+			return 'build/soy';
+		});
+}
