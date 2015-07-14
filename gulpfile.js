c620a3bf4df6617943bd5b72269b4f9f2d92f89d
Maira Bello <maira.araujo@liferay.com>
2015-07-14 15:32:09 -0300

    Updates version of gulp-metal

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,7 @@
 'use strict';
 
-var combiner = require('stream-combiner');
 var gulp = require('gulp');
-var lazypipe = require('lazypipe');
 var metal = require('gulp-metal');
-var path = require('path');
 
 metal.registerTasks({
 	buildSrc: 'bower_components/steel-*/src/**/*.js',
@@ -15,25 +12,4 @@ metal.registerTasks({
 	scssSrc: 'bower_components/steel-*/src/**/*.scss'
 });
 
-gulp.task('soy', ['soy:crystal'], function() {
-	return gulp.src('bower_components/steel-*/src/**/*.soy')
-		.pipe(compileSoy());
-});
-
-gulp.task('soy:crystal', function() {
-	return gulp.src('bower_components/crystal-*/src/**/*.soy')
-		.pipe(compileSoy());
-});
-
 gulp.task('default', ['build:globals', 'build:amd', 'build:jquery']);
-
-function compileSoy() {
-	return combiner(
-		metal.soy.generateSoy(),
-		gulp.dest(function(file) {
-			var index = file.relative.indexOf(path.sep + 'src' + path.sep);
-			file.base = path.join(file.base, file.relative.substr(0, index + 5));
-			return 'build/soy';
-		})
-	);
-}
